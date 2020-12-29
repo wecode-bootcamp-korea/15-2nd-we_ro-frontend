2a22cdd4435244ce4ad47839a1913031b8644802
aramcho206 <aramcho206@gmail.com>
2020-12-29 18:23:11 +0900

    Add: main changes

--- src/Routes.js
+++ src/Routes.js
@@ -9,25 +9,25 @@ import SignIn from "./Pages/SignIn/SignIn";
 import SignUp from "./Pages/SignUp/SignUp";
 import Main from "./Pages/Main/Main";
 import Storage from "./Pages/Storage/Storage";
-import GenreDetail from "./Pages/GenreDetail/GenreDetail"
-import SituationDetail from "./Conponents/SituationDetail/SituationDetail"
+import GenreDetail from "./Pages/GenreDetail/GenreDetail";
+import SituationDetail from "./Conponents/SituationDetail/SituationDetail";
 class Routes extends Component {
   render() {
     return (
       <Router>
-          <Nav/>
+        <Nav />
         <Switch>
-            <Route exact path="/" component={Main}/>
-            <Route exact path="/albumDetail" component={AlbumDetail}/>
-            <Route exact path="/signIn" component={SignIn}/>
-            <Route exact path="/signUp" component={SignUp}/>
-            <Route exact path="/chart" component={Chart}/>
-            <Route exact path="/storage" component={Storage}/>
-            <Route exact path="/genredetail" component={GenreDetail}/>
-            <Route exact path="/situationdetail" component={SituationDetail}/>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/albumDetail" component={AlbumDetail} />
+          <Route exact path="/signIn" component={SignIn} />
+          <Route exact path="/signUp" component={SignUp} />
+          <Route exact path="/chart" component={Chart} />
+          <Route exact path="/storage" component={Storage} />
+          <Route exact path="/genredetail" component={GenreDetail} />
+          <Route exact path="/situationdetail" component={SituationDetail} />
         </Switch>
-        <Footer/>
-        <PlayBar/>
+        <Footer />
+        <PlayBar />
       </Router>
     );
   }
