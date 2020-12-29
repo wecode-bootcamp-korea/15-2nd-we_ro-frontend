d5453dce76ef1c11d7b9294921bed76418fcf053
김영광 <kim-young-gwang@gim-yeong-gwang-ui-MacBookPro.local>
2020-12-29 18:41:07 +0900

    공통 Styled-components 설정

--- src/config.js
+++ src/config.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const config = () => {
-    return (
-        <div>
-            hi
-        </div>
-    );
+  return <div>hi</div>;
 };
 
 export default config;
---
+++ src/styles/Theme.js
@@ -0,0 +1,9 @@
+import styled, { css } from "styled-components";
+
+const theme = {
+  mainColor: "black",
+  dangerColor: "black",
+  successColor: "black",
+};
+
+export default theme;
