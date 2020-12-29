4bdbc8961a8e532ef669e68086b0f3a71fd42055
김영광 <kim-young-gwang@gim-yeong-gwang-ui-MacBookPro.local>
2020-12-29 18:57:43 +0900

    공통 Styled-components 설정

--- src/Components/PlayBar/PlayBar.js
+++ src/Components/PlayBar/PlayBar.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const PlayBar = () => {
-    return (
-        <div>
-            hi
-        </div>
-    );
+  return <div>hi</div>;
 };
 
 export default PlayBar;
